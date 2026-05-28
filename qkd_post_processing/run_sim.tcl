0f4b27df13605e60755987785735ead71190d559
SangNmd2004 <158246508+SangNmd2004@users.noreply.github.com>
2026-05-28 13:38:12 +0700

    Delete qkd_post_processing/run_sim.tcl

--- qkd_post_processing/run_sim.tcl
+++
@@ -1,6 +0,0 @@
-open_project D:/XilinxProjects/QKD/QKD.xpr
-set_property source_mgmt_mode All [current_project]
-update_compile_order -fileset sim_1
-launch_simulation -step all
-run all
-close_project
